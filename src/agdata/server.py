6eee2898baf45d573a6f9ded1f12efad1e8b60c2
Mason Earles <jmearles@JME-LM001.local>
2021-09-07 14:21:09 -0700

    upload and download data from AWS S3 functionality in server.py

--- src/agdata/server.py
+++ src/agdata/server.py
@@ -17,26 +17,42 @@ class AgDataServer:
         self.agdata_s3_uri = 'https://s3.us-west-1.amazonaws.com/agdata-data/'
 
 
-    def put_data(self, dataset_dir, dataset_name):
+    def upload_dataset(self, dataset_dir, dataset_name):
 
-        # Upload the zipped dataset file
-        # s3 = boto3.resource('s3')
-        # data = open(dataset_dir + '/' + dataset_name + '.zip', 'rb')
-        # s3.Bucket('agdata-data').put_object(Key=dataset_name + '.zip', Body=data)
-
+        # Establish connection with s3 via boto
         self.s3 = boto3.client('s3')
-        self.upload(bucket='agdata-data',
-                    key=dataset_name + '.zip',
-                    file=dataset_dir + '/' + dataset_name + '.zip')
 
-    def upload_callback(self, size):
-        self.pg.update(self.pg.currval + size)
-
-    def upload(self, bucket, key, file):
+        # Setup progress bar
         self.pg = progressbar.progressbar.ProgressBar(
-            maxval=os.stat(file).st_size)
+            maxval=os.stat(dataset_dir + '/' + dataset_name + '.zip').st_size)
         self.pg.start()
 
-        with open(file, 'rb') as data:
-            self.s3.upload_fileobj(
-                data, bucket, key, Callback=self.upload_callback)
+        # Upload data to agdata-data bucket
+        with open(dataset_dir + '/' + dataset_name + '.zip', 'rb') as data:
+            self.s3.upload_fileobj(data, 
+                                   bucket='agdata-data', 
+                                   key=dataset_name + '.zip', 
+                                   Callback=self.updownload_callback)   
+
+
+    def download_dataset(self, dest_dir, dataset_name):
+
+        # Establish connection with s3 via boto
+        self.s3 = boto3.client('s3')
+        self.s3_resource = boto3.resource('s3')
+
+        # Setup progress bar
+        self._size = float(self.s3_resource.ObjectSummary(bucket_name='agdata-data', key=dataset_name + '.zip').size)
+        self.pg = progressbar.progressbar.ProgressBar(maxval=self._size)
+        self.pg.start()
+
+        # Upload data to agdata-data bucket
+        with open(dest_dir + '/' + dataset_name + '.zip', 'wb') as data:
+            self.s3.download_fileobj(Bucket='agdata-data', 
+                                     Key=dataset_name + '.zip', 
+                                     Fileobj=data,
+                                     Callback=self.updownload_callback)  
+
+
+    def updownload_callback(self, size):
+        self.pg.update(self.pg.currval + size)
