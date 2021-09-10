95995821106157c8c17670df03dbf5b1cc9fbeb3
Mason Earles <jmearles@JME-LM001.local>
2021-09-10 11:06:21 -0700

    Remove data-preprocessing directory and contents. Preprocessing code for rangeland_weeds_australia done

--- src/agdata/preprocess.py
+++ src/agdata/preprocess.py
@@ -21,9 +21,9 @@ class PreprocessData:
 
         if dataset_name == 'rangeland_weeds_australia':
             dataset_dir = self.data_original_dir + dataset_name + '/'
+            imgs_dir = dataset_dir + 'images/'
             labels_dir = dataset_dir + 'labels/'
             labels_path = labels_dir + 'labels.csv'
-            labels = []
             labels_unique = []
 
             # Make directories with class names
@@ -31,10 +31,13 @@ class PreprocessData:
                 next(f)
                 labels = [row.split(',')[2] for row in f]
 
+            with open(labels_path) as f:
+                next(f)
+                img_names = [row.split(',')[0].strip().replace(' ', '_') for row in f]
+
             # Read through list, keep only unique classes, and create directories for each class name
-            for label in labels:
+            for k, label in enumerate(labels):
                 if label not in labels_unique:
                     labels_unique.append(label)
                     os.mkdir(labels_dir + label)
-
-            # Move files to folder with correct class names
+                os.rename(imgs_dir + img_names[k], labels_dir + label + '/' + img_names[k])
