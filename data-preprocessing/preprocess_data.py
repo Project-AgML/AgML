fd944ec22d16e467199526bca4024b4b0b84aafd
Mason Earles <jmearles@JME-LM001.local>
2021-09-10 10:40:37 -0700

    merge mason WIP

--- data-preprocessing/preprocess_data.py
+++ data-preprocessing/preprocess_data.py
@@ -3,39 +3,36 @@ import csv
 
 class PreprocessData:
 
-    def __init__(self, data_dir, dataset_name):
+    def __init__(self, data_dir):
         self.data_dir = data_dir
         self.data_original_dir = self.data_dir + 'original/'
         self.data_processed_dir = self.data_dir + 'processed/'
 
-        self.dataset_name = None
-    
-    def pp_bean_disease_uganda(self):
-        dataset_name = 'bean_disease_uganda'
-    
-    def pp_carrot_weeds_germany(self):
-        dataset_name = 'carrot_weeds_germany'
+    def preprocess(self, dataset_name):
 
-    def pp_carrot_weeds_macedonia(self):
-        dataset_name = 'carrot_weeds_macedonia'
+        if dataset_name == 'bean_disease_uganda':
+            None
+        
+        if dataset_name == 'carrot_weeds_germany':
+            None
 
-    def pp_rangeland_weeds_australia(self):
-        dataset_name = 'rangeland_weeds_australia'
-        dataset_dir = self.data_original_dir + '/' + self.dataset_name + '/'
-        labels = []
-        labels_unique = []
+        if dataset_name == 'carrot_weeds_macedonia':
+            None
 
-        # Make directories with class names
-        with open(dataset_dir) as f:
-            labels.append(csv.reader(f))
+        if dataset_name == 'rangeland_weeds_australia':
+            dataset_name = 'rangeland_weeds_australia'
+            dataset_dir = self.data_original_dir + '/' + dataset_name + '/'
+            labels = []
+            labels_unique = []
 
-        # Read through list, keep only unique classes, and create directories for each class name
-        for label in labels:
-            if label not in labels_unique:
-                labels_unique.append(label)
-            
+            # Make directories with class names
+            with open(dataset_dir) as f:
+                labels.append(csv.reader(f))
 
-        # Move files to folder with correct class names
+            # Read through list, keep only unique classes, and create directories for each class name
+            for label in labels:
+                if label not in labels_unique:
+                    labels_unique.append(label)
+                    os.mkdir(dataset_dir + label)
 
-        # dataset_dir = os.listdir(self.data_original_dir + '/' + self.dataset_name + '/' + labels)
-
+            # Move files to folder with correct class names
