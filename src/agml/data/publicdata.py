572b2c6b8ae52ba34ca801811444dd9c83756748
amogh7joshi <joshi.amoghn@gmail.com>
2021-09-11 10:14:52 -0400

    Updated publicdata API and path info

--- src/agml/data/publicdata.py
+++ src/agml/data/publicdata.py
@@ -1,26 +1,25 @@
-import json
 import os
 import sys
+import json
 import zipfile
+import warnings
+
 import boto3
 import progressbar
 
-class PublicDataAPI: 
-
+class PublicDataAPI:
     """
-    A class for working with AgData
+    A public API for working with AgML data.
 
-    ...
     Attributes
     ----------
     No user specified attributes
-
     """
-
     def __init__(self):
-
-        # Read in meta-data for data sources file
-        self.data_srcs_path = 'assets/public_datasources.json'
+        # Read in metadata for data sources file
+        self.data_srcs_path = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            'assets', 'public_datasources.json')
         with open(self.data_srcs_path) as f:
             self.data_srcs = json.load(f)
 
@@ -30,11 +29,9 @@ class PublicDataAPI:
         # Initialize attribute for storing dataset download path
         self.dataset_download_path = None
 
-
     def upload_dataset(self, dataset_dir, dataset_name):
-
         """
-        Uploads dataset to agdata-data s3 file storage
+        Uploads dataset to agdata-data s3 file storage.
         
         Parameters
         ----------
@@ -43,30 +40,29 @@ class PublicDataAPI:
             dataset_name : str
                 name of dataset (without '.zip') -- for list of datasets run self.data_srcs.keys()
         """
-
         # Establish connection with s3 via boto
         self.s3 = boto3.client('s3')
 
         # Setup progress bar
-        self.pg = progressbar.progressbar.ProgressBar(
-            maxval=os.stat(dataset_dir + '/' + dataset_name + '.zip').st_size)
-        self.pg.start()
+        pg = progressbar.progressbar.ProgressBar(
+            maxval=os.stat(os.path.join(dataset_dir, dataset_name + '.zip')).st_size)
+        pg.start()
 
         # Upload data to agdata-data bucket
         try:
             with open(dataset_dir + '/' + dataset_name + '.zip', 'rb') as data:
                 self.s3.upload_fileobj(Fileobj=data, 
-                                    Bucket='agdata-data', 
-                                    Key=dataset_name + '.zip', 
-                                    Callback=self.updownload_callback)
+                                       Bucket='agdata-data',
+                                       Key=dataset_name + '.zip',
+                                       Callback=lambda x: pg.update(pg.currval + x))
         except:
-            print('Upload unsuccessful. You may not have permission to upload to the agdata-data s3 bucket...')
-
+            warnings.warn(
+                f'Upload of {dataset_name} unsuccessful. You may not have permission '
+                f'to upload to the agdata-data s3 bucket.', category = UserWarning)
 
     def download_dataset(self, dataset_name, dest_dir):
-
         """
-        Downloads dataset from agdata-data s3 file storage
+        Downloads dataset from agdata-data s3 file storage.
         
         Parameters
         ----------
@@ -75,41 +71,32 @@ class PublicDataAPI:
             dest_dir : str
                 path for saving downloaded dataset
         """
-
         # Establish connection with s3 via boto
         self.s3 = boto3.client('s3')
         self.s3_resource = boto3.resource('s3')
 
         # Setup progress bar
-        self._size = float(self.s3_resource.ObjectSummary(bucket_name='agdata-data', key=dataset_name + '.zip').size)
-        self.pg = progressbar.progressbar.ProgressBar(maxval=self._size)
-        self.pg.start()
+        self._size = float(self.s3_resource.ObjectSummary(
+            bucket_name='agdata-data', key=dataset_name + '.zip').size)
+        pg = progressbar.progressbar.ProgressBar(maxval=self._size)
+        pg.start()
 
         # File path of zipped dataset
-        self.dataset_download_path = dest_dir + '/' + dataset_name + '.zip'
+        self.dataset_download_path = os.path.join(dest_dir, dataset_name + '.zip')
 
         # Upload data to agdata-data bucket
         with open(self.dataset_download_path, 'wb') as data:
             self.s3.download_fileobj(Bucket='agdata-data', 
                                      Key=dataset_name + '.zip', 
                                      Fileobj=data,
-                                     Callback=self.updownload_callback)
+                                     Callback=lambda x: pg.update(pg.currval + x))
 
         # Unzip downloaded dataset
-        with zipfile.ZipFile(self.dataset_download_path, 'r') as zip:
-            zip.printdir()
+        with zipfile.ZipFile(self.dataset_download_path, 'r') as z:
+            z.printdir()
             print('Extracting files...')
-            zip.extractall(path=dest_dir)
+            z.extractall(path=dest_dir)
             print('Done!')
 
         # Delete zipped file
         os.remove(self.dataset_download_path)
-
-
-    def updownload_callback(self, size):
-
-        """
-        Callback function for tracking download or upload progress
-        """
-        
-        self.pg.update(self.pg.currval + size)
