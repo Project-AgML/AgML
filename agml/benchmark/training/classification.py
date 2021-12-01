ef6187b8ab661a4903ab9642b79e7776f15a85a4
amogh7joshi <joshi.amoghn@gmail.com>
2021-12-01 17:47:11 -0500

    Added normalization

--- agml/benchmark/training/classification.py
+++ agml/benchmark/training/classification.py
@@ -14,12 +14,12 @@
 
 import os
 import sys
+from enum import Enum
 import argparse
 
 import torch
 import torch.nn as nn
 from torchvision.models import efficientnet_b4
-from torchmetrics.functional import accuracy
 
 from tqdm import tqdm
 import albumentations as A
@@ -169,7 +169,7 @@ class Trainer(object):
             if log:
                 with open(log_file, 'a') as f: # noqa
                     f.write(f"Epoch {epoch}, "
-                            f"Average Loss: {avg_epoch_loss:.4f}, "
+                            f"Average Loss: {train_loss:.4f}, "
                             f"Epoch Loss: {final_loss:.4f}, "
                             f"Validation Loss: {final_val_loss:.4f}\n")
 
@@ -184,6 +184,86 @@ class Trainer(object):
                         if os.path.exists(path):
                             os.remove(path)
             self._saved_checkpoints[save_path] = final_val_loss
+
+
+class Summary(Enum):
+    NONE = 0
+    AVERAGE = 1
+    SUM = 2
+    COUNT = 3
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f', summary_type=Summary.AVERAGE):
+        self.name = name
+        self.fmt = fmt
+        self.summary_type = summary_type
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+    def summary(self):
+        fmtstr = ''
+        if self.summary_type is Summary.NONE:
+            fmtstr = ''
+        elif self.summary_type is Summary.AVERAGE:
+            fmtstr = '{name} {avg:.3f}'
+        elif self.summary_type is Summary.SUM:
+            fmtstr = '{name} {sum:.3f}'
+        elif self.summary_type is Summary.COUNT:
+            fmtstr = '{name} {count:.3f}'
+        else:
+            raise ValueError('invalid summary type %r' % self.summary_type)
+
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def display_summary(self):
+        entries = [" *"]
+        entries += [meter.summary() for meter in self.meters]
+        print(' '.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+def accuracy(output, target):
+    """Computes the accuracy between `output` and `target`."""
+    with torch.no_grad():
+        batch_size = target.size(0)
+        _, pred = torch.topk(output, 1, 1)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+        correct_k = correct[:1].reshape(-1).float().sum(0, keepdim = True)
+        return correct_k.mul_(100.0 / batch_size)
 
 
 def execute():
