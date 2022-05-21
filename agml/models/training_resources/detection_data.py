a62d491dec9a69fcafdb907024a66833b0f42027
amogh7joshi <joshi.amoghn@gmail.com>
2022-05-21 15:23:55 -0400

    Added augmentations to detection data loading

--- agml/models/training_resources/detection_data.py
+++ agml/models/training_resources/detection_data.py
@@ -17,6 +17,9 @@ from typing import List, Union, Any
 import numpy as np
 from PIL import Image
 
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
 import torch
 from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
@@ -83,9 +86,73 @@ def _post_prepare_for_efficientdet(image, annotation):
     return image, target
 
 
+def _default_augmentation(image_size = 512):
+    """Default training and validation augmentations."""
+    # Default augmentation function.
+    def _augmentation(image, annotation):
+        nonlocal image_size
+
+        # Construct the sample.
+        sample = {
+            "image": np.array(image, dtype = np.float32),
+            "bboxes": annotation['bboxes'],
+            "labels": annotation['labels']}
+
+        # Augment the sample.
+        sample = A.Compose(
+            [A.Resize(height = image_size, width = image_size, p = 1),
+             ToTensorV2(p = 1)], p = 1.0,
+            bbox_params = A.BboxParams(
+                format = "pascal_voc", min_area = 0,
+                min_visibility = 0, label_fields = ["labels"]))(**sample)
+
+        # Return the sample.
+        return sample['image'], {'bboxes': sample['bboxes'],
+                                 'labels': sample['labels']}
+
+    # Return the default augmentation function.
+    return _augmentation
+
+
+def transformation(augmentation: Any = None,
+                   image_size: int = 512):
+    """Applies the full EfficientDet data transform to the loader."""
+    # Get the default augmentation if none is passed.
+    if augmentation is None:
+        augmentation = _default_augmentation(image_size = image_size)
+
+    # If a dictionary is passed, then make separate transforms
+    # for train and validation.
+    if isinstance(augmentation, dict):
+        def _apply_train(image, annotation):
+            nonlocal augmentation
+            image, annotation = _pre_prepare_for_efficientdet(image, annotation)
+            image, annotation = augmentation['train'](image, annotation)
+            image, annotation = _post_prepare_for_efficientdet(image, annotation)
+            return image, annotation
+
+        def _apply_val(image, annotation):
+            nonlocal augmentation
+            image, annotation = _pre_prepare_for_efficientdet(image, annotation)
+            image, annotation = augmentation['val'](image, annotation)
+            image, annotation = _post_prepare_for_efficientdet(image, annotation)
+            return image, annotation
+
+        return {'train': _apply_train, 'val': _apply_val}
+
+    # Otherwise, return a single universal transform.
+    else:
+        def _apply(image, annotation):
+            nonlocal augmentation
+            image, annotation = _pre_prepare_for_efficientdet(image, annotation)
+            image, annotation = augmentation(image, annotation)
+            image, annotation = _post_prepare_for_efficientdet(image, annotation)
+            return image, annotation
+        return _apply
+
+
 def build_loader(dataset: Union[List[str], str],
-                 batch_size: int = 4,
-                 augmentations: Any = None) -> agml.data.AgMLDataLoader:
+                 batch_size: int = 4) -> agml.data.AgMLDataLoader:
     """Constructs an `AgMLDataLoader` for object detection.
 
     Given either a single dataset or a list of datasets, this method will
@@ -96,12 +163,6 @@ def build_loader(dataset: Union[List[str], str],
     pl.seed_everything(2499751)
     loader = agml.data.AgMLDataLoader(dataset)
     loader.shuffle()
-
-    # Apply the transforms to the loader.
-    loader.transform(dual_transform = _pre_prepare_for_efficientdet)
-    if augmentations is not None:
-        loader.transform(dual_transform = augmentations)
-    loader.transform(dual_transform = _post_prepare_for_efficientdet)
 
     # Apply the batch size.
     loader.batch(batch_size = batch_size)
@@ -116,12 +177,22 @@ class EfficientDetDataModule(pl.LightningDataModule):
 
     def __init__(self,
                  loader: agml.data.AgMLDataLoader,
+                 augmentation: Any,
                  num_workers: int = 8):
         # Get the training and validation `AgMLDataLoader`s.
         self._train_loader = loader.train_data
         self._train_loader.as_torch_dataset()
         self._val_loader = loader.val_data
         self._val_loader.as_torch_dataset()
+
+        # Update the transforms.
+        if isinstance(augmentation, dict):
+            self._train_transform = augmentation['train']
+            self._val_transform = augmentation['val']
+        else:
+            self._train_transform = self._val_transform = augmentation
+        self._train_loader.transform(self._train_transform)
+        self._val_transform.transform(self._val_transforms)
 
         # Initialize the base module.
         self._num_workers = num_workers
