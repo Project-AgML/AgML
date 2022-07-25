Fixes: detection models, mean average precision, etc

agml/models/detection.py
@@ -57,7 +57,7 @@ class DetectionModel(AgMLModelBase):
     `configure_optimizers`, etc. See PyTorch Lightning for more information.
     """
     serializable = frozenset(("model", "confidence_threshold", "source"))
-    state_override = frozenset(("model", ))
+    state_override = frozenset(("model",))
 
     def __init__(self, dataset = None, conf_threshold = 0.3, **kwargs):
         # Initialize the base modules.
@@ -75,7 +75,7 @@ class DetectionModel(AgMLModelBase):
         warnings.filterwarnings(
             'ignore', category = UserWarning, module = 'ensemble_boxes')
         warnings.filterwarnings(
-            'ignore', category = UserWarning, module = 'effdet.bench') # noqa
+            'ignore', category = UserWarning, module = 'effdet.bench')  # noqa
 
     @auto_move_data
     def forward(self, batch):
@@ -105,7 +105,7 @@ class DetectionModel(AgMLModelBase):
         self.model.load_state_dict(state)
 
     @property
-    def original(self): # override for detection models.
+    def original(self):  # override for detection models.
         return self.model.model
 
     @torch.jit.ignore()
@@ -215,7 +215,7 @@ class DetectionModel(AgMLModelBase):
         if return_shapes:
             return images, shapes
         return images
-    
+
     def _process_detections(self, detections):
         """Post-processes the output detections (boxes, labels) from the model."""
         predictions = []

agml/models/training_resources/detection_modeling.py
@@ -55,7 +55,7 @@ class DetectionTrainingModel(DetectionModel):
     @staticmethod
     def make_model(num_classes: int,
                    pretrained_weights: str,
-                   image_size: Union[int, tuple] = 1024,
+                   image_size: Union[int, tuple] = 512,
                    architecture: str = 'tf_efficientdet_d4'):
         """Constructs the `EfficientDet` model from the provided parameters."""
         # Parse the input arguments.
@@ -118,7 +118,7 @@ class DetectionTrainingModel(DetectionModel):
         return scaled_boxes
 
     def configure_optimizers(self):
-        return torch.optim.AdamW(self.model.parameters(), lr = self.lr)
+        return torch.optim.Adam(self.model.parameters(), lr = self.lr)
 
     def training_step(self, batch, batch_idx):
         # Run a forward pass through the model.

agml/models/training_resources/map_torch.py
@@ -46,14 +46,14 @@ def _add_truth_scores_to_annotations(annotations):
 def intersection_over_union(boxes_preds, boxes_labels):
     """Calculates the intersection-over-union between boxes."""
     # Extract the top-left and bottom-right coordinates.
-    box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
-    box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
-    box1_x2 = boxes_preds[..., 0:1] + boxes_preds[..., 2:3] / 2
-    box1_y2 = boxes_preds[..., 1:2] + boxes_preds[..., 3:4] / 2
-    box2_x1 = boxes_labels[..., 0:1] - boxes_labels[..., 2:3] / 2
-    box2_y1 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
-    box2_x2 = boxes_labels[..., 0:1] + boxes_labels[..., 2:3] / 2
-    box2_y2 = boxes_labels[..., 1:2] + boxes_labels[..., 3:4] / 2
+    box1_x1 = boxes_preds[..., 0:1] # - boxes_preds[..., 2:3] / 2
+    box1_y1 = boxes_preds[..., 1:2] # - boxes_preds[..., 3:4] / 2
+    box1_x2 = boxes_preds[..., 0:1] + boxes_preds[..., 2:3] # / 2
+    box1_y2 = boxes_preds[..., 1:2] + boxes_preds[..., 3:4] # / 2
+    box2_x1 = boxes_labels[..., 0:1] # - boxes_labels[..., 2:3] / 2
+    box2_y1 = boxes_labels[..., 1:2] # - boxes_labels[..., 3:4] / 2
+    box2_x2 = boxes_labels[..., 0:1] + boxes_labels[..., 2:3] # / 2
+    box2_y2 = boxes_labels[..., 1:2] + boxes_labels[..., 3:4] # / 2
     x1 = torch.max(box1_x1, box2_x1)
     y1 = torch.max(box1_y1, box2_y1)
     x2 = torch.min(box1_x2, box2_x2)
@@ -240,7 +240,7 @@ class MeanAveragePrecision(object):
         # Create the data in the proper format.
         for bbox, label in zip(gt_boxes, gt_labels):
             self._ground_truth_data.append(
-                [self._num_updates, int(label - 1), *bbox])
+                [self._num_updates, int(label - 1), 1.0, *bbox])
         if pred_boxes.ndim == 1:
             pred_boxes = np.expand_dims(pred_boxes, axis = 0)
         for bbox, label, score in zip(pred_boxes, pred_labels, pred_scores):