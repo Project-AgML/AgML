189abf4d1e51221c7e8edb05b36c0b41272e583b
Amogh Joshi <joshi.amoghn@gmail.com>
2024-01-04 14:13:05 -0500

    Push to version 0.6.0

--- tests/main.py
+++ tests/main.py
@@ -21,6 +21,7 @@ BASE_PATH = os.path.join(os.path.expanduser('~'), '.agml', 'datasets')
 _USED_DATASETS = ['bean_disease_uganda', 'apple_flower_segmentation', 'apple_detection_usa']
 _PREEXISTING_DATASETS = []
 
+
 def _find_preexisting_datasets():
     global _USED_DATASETS, _PREEXISTING_DATASETS, BASE_PATH
     for dataset in _USED_DATASETS:
@@ -28,26 +29,27 @@ def _find_preexisting_datasets():
                 and os.path.isdir(os.path.join(BASE_PATH, dataset)):
             _PREEXISTING_DATASETS.append(dataset)
 
+
 def _remove_new_datasets():
     global _USED_DATASETS, _PREEXISTING_DATASETS, BASE_PATH
     for dataset in _USED_DATASETS:
         if dataset not in _PREEXISTING_DATASETS:
             shutil.rmtree(dataset)
 
+
 def pre_test_configure():
     _find_preexisting_datasets()
 
+
 def post_test_cleanup():
     _remove_new_datasets()
+
 
 def execute_tests():
     pre_test_configure()
     pytest.main()
     post_test_cleanup()
 
+
 if __name__ == '__main__':
     execute_tests()
-
-
-
-
