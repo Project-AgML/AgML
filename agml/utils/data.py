cab1567549a267be501977bc6d6fcd57c1a15897
Amogh Joshi <joshi.amoghn@gmail.com>
2025-04-10 13:35:01 -0400

    Packaging issues fixed

--- agml/utils/data.py
+++ agml/utils/data.py
@@ -98,7 +98,7 @@ def copyright_print(name, location=None):
         "Apache 2.0": "https://www.apache.org/licenses/LICENSE-2.0",
         'CC BY-NC 4.0': 'https://creativecommons.org/licenses/by-nc/4.0/',
         'CC BY-NC-SA 4.0': 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
-        'CC BY 4.0': 'https://creativecommons.org/licenses/by/4.0/deed.en'}
+        'CC BY 4.0': 'https://creativecommons.org/licenses/by/4.0/deed.en'
     }
 
     # Create license message
